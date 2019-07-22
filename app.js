f25e05f97b0e866ed138f6d8a4d682c27fd1a2b2
Nikita <43913084+lerowhou@users.noreply.github.com>
2019-07-22 21:23:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const second = 1000,
       minute = second * 60,
       hour = minute * 60,
-      day = hour * 24;
+      day = hour * 24,
+      week = day * 7;
 
 let countDown = new Date('Dec 31, 2060 00:00:00').getTime(),
     x = setInterval(function() {
@@ -9,15 +10,9 @@ let countDown = new Date('Dec 31, 2060 00:00:00').getTime(),
       let now = new Date().getTime(),
           distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('weeks').innerText = Math.floor(distance / (week)),  
+        document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-      
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
-
     }, second)
